0b0858b7c790b4700f398351ae616e302922bcca
Сat Cheshire <catmagic000@gmail.com>
2023-09-12 06:17:54 +0300

    nya

--- HW2/task2/src/Main.java
+++ HW2/task2/src/Main.java
@@ -1,17 +1,25 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое целое число: ");
+        int number1 = scanner.nextInt();
+        System.out.print("Введите второе целое число: ");
+        int number2 = scanner.nextInt();
+        try{
+            if(number2==0)
+            {
+                throw new DivisionByZeroException("Деление на ноль недопустимо"");
+            }
+            System.out.print( number1/number2);
+        }
+        catch(DivisionByZeroException ex)
+        {
+            System.out.print(ex.getMessage());
         }
     }
 }
